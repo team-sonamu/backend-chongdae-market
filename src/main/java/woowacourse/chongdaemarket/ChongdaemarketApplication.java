2661bf8be697e8151a077a8ce2a05d28dfe104ea
Dora Choo <choo000407@naver.com>
2024-07-15 20:13:42 +0900

    chore: 초기 환경 설정 추가 (#3)

---
+++ src/main/java/woowacourse/chongdaemarket/ChongdaemarketApplication.java
@@ -0,0 +1,13 @@
+package woowacourse.chongdaemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChongdaemarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChongdaemarketApplication.class, args);
+	}
+
+}
