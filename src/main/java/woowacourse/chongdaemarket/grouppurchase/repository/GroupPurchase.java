e7d753e982c01695ef8788329abbcefd7527e8bf
SCY <helenason@naver.com>
2024-07-15 20:52:08 +0900

    feat: 공동구매 도메인 추가

---
+++ src/main/java/woowacourse/chongdaemarket/grouppurchase/repository/GroupPurchase.java
@@ -0,0 +1,61 @@
+package woowacourse.chongdaemarket.grouppurchase.repository;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import woowacourse.chongdaemarket.member.repository.Member;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+public class GroupPurchase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Member member;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String description;
+
+    private String thumbnailUrl;
+
+    private String productUrl;
+
+    @NotNull
+    private LocalDateTime deadline;
+
+    @NotNull
+    private String meetingAddress;
+
+    private String meetingAddressDetail;
+
+    @NotNull
+    private Integer currentCount;
+
+    @NotNull
+    private Integer totalCount;
+
+    @NotNull
+    private Boolean isManualConfirmed;
+
+    @NotNull
+    private BigDecimal totalPrice;
+}
